5f9e53904f5a2ad0a67102cb2a9d6f59bcfd9584
John <lol.hsiao@gmail.com>
2016-05-20 09:09:38 +0800

    學生地址導航至學校

--- WebContent/inc/js/develop/stdinfo.js
+++ WebContent/inc/js/develop/stdinfo.js
@@ -6,12 +6,68 @@ function getDilgInfo(no, name, Oid){
 	}, "json");	
 }
 
+var lookout=false;
 function getStdContectInfo(no, name){
 	$("#stdNameNo").text(no+" - "+name);
 	$.get("/eis/getStdContectInfo",{student_no:no},
-		function(data){
-		$("#info").html(data.info);				
-	}, "json");	
+			function(data){
+			$("#info").html(data.info);				
+		}, "json");	
+	$.fn.tinyMapConfigure({
+	    'api': 'https://maps.googleapis.com/maps/api/js',
+	    'v': '3.3',
+	    'language': 'zh‐TW'
+	});
+	
+	
+	setTimeout(function() {			
+			$.ajax({
+				type:"POST",
+				url:"/eis/getStdsgeo",
+				dataType:"json",
+				data:{stdNo:no},
+				success:function(d){
+					if(!lookout){
+						lookout=true;
+						obj = jQuery.parseJSON(d.list[0].geocode);
+						$("#stdMap").tinyMap({
+							direction: [{
+							                id: no,
+							                from: [obj.lat, obj.lng],
+							                // 起點的替代顯示文字
+							                fromText: 'no',
+							                to: ['25.032', '121.609'],
+							                toText: '中華科大',	
+							                optimize: true,
+							                travel: 'driving',
+							                autoViewport: true,
+							            }]
+						});
+					}else{
+						obj = jQuery.parseJSON(d.list[0].geocode);
+						$("#").tinyMap('clear');
+						$("#stdMap").tinyMap('modify',{
+							direction: [{
+							                id: no,
+							                from: [obj.lat, obj.lng],
+							                // 起點的替代顯示文字
+							                fromText: 'no',
+							                to: ['25.032', '121.609'],
+							                toText: '中華科大',	
+							                optimize: true,
+							                travel: 'driving',
+							                autoViewport: true,
+							            }]
+						});
+					}
+				}
+				
+			});
+		
+		
+	}, 500);
+	
+		
 }
 
 function getStdScoreInfo(no, name){
